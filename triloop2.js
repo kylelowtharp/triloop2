b4e11abd6925b8040749a890b608f65cf645e7cc
kylelowtharp <klv9700@saintjoe.edu>
2015-01-26 12:14:05 -0500

    updates chapter 2 comments

--- triloop2.js
+++ triloop2.js
@@ -1,5 +1,15 @@
+*/Kyle Lowtharp triloop homework/*
 
 
-Use this for homework
-This Is my proof
+
+
+Hashes = '#';
+
+
+ for (var number = 1 ; number <= 10; number = number + 1){
+
+	   console.log(hashes);
+	   hashes += '#';
+ }
+
 
